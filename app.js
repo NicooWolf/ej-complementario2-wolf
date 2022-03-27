376279c63baf6fe6e5afaf7968ee0382310b6d69
Nicolás Wolf <nico.wolf@outlook.com>
2022-03-27 20:20:21 -0300

    Complementario 2

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-let nombre = prompt("Ingrese su nombre por favor");
-alert(`Bienvenido ${nombre}!`);
-let valor1 = Number(prompt("Ingrese el primer numero"));
-let valor2 = Number(prompt("Ingrese el segundo numero"));
-let valor3 = Number(prompt("Ingrese el tercer numero"));
-
-function promediar() {
-  let res = (valor1 + valor2 + valor3) / 3;
-  if (res > 18) {
-    document.write(`El promedio es ${res}, y es de una persona mayor de edad.`);
-  } else
-    document.write(`El promedio es ${res}, y es de una persona menor de edad.`);
-  while (res > 18) {
-    res++;
-    document.write(`<br> Sumandole 1, su promedio se convierte en: ${res}`);
-    break;
+function saludar(nombre) {
+  nombre = prompt("Ingrese su nombre por favor");
+  return alert(`Bienvenido a la Parrilla Nueva Adelina ${nombre}!`);
+}
+function carrito() {
+  let entrada = prompt(
+    "Ingresa el producto que desees agregar a tu lista por favor. Ingresa 'ESC' para salir"
+  ).toUpperCase();
+  const productos = [];
+  while (entrada != "ESC") {
+    productos.push(entrada);
+    entrada = prompt(
+      "Ingresa otro producto si asi lo deseas, o 'ESC' para salir"
+    ).toUpperCase();
+  }
+  document.write("Su carrito contiene lo siguiente: <br>");
+  for (let i = 0; i < productos.length; i++) {
+    document.write(productos[i], "<br>");
   }
 }
 
-promediar();
+saludar();
+carrito();
+
+// let arr = [6, 5, 0, 4, 3, 2, 0, 1, 0];
+// function revertir() {
+//   return arr.reverse();
+// }
+// document.write(revertir(), "<br>");
+// let arro = arr.filter(function (array) {
+//   return array !== 0;
+// });
+
+// document.write(arro);
